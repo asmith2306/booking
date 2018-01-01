8909c6d1b295f634c3fd78ef7cc9ff0e4e1e3fea
Alan <Alan@Home-Laptop.Home>
2018-01-01 16:08:32 +0000

    Added routing module

--- booking-frontend/src/main/angular/src/app/app.module.ts
+++ booking-frontend/src/main/angular/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
