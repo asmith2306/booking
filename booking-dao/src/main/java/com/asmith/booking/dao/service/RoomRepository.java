19315d2821ba6e6cec9d89534bae58ee1aa5b715
Alan <Alan@Home-Laptop.Home>
2018-01-14 21:46:23 +0000

    Added room functionality

--- booking-dao/src/main/java/com/asmith/booking/dao/service/RoomRepository.java
+++ booking-dao/src/main/java/com/asmith/booking/dao/service/RoomRepository.java
@@ -1,9 +1,18 @@
 package com.asmith.booking.dao.service;
 
 import com.asmith.booking.entities.Room;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * @author asmith
  */
-public interface RoomRepository extends CrudRepository<Room, Long> {}
+public interface RoomRepository extends CrudRepository<Room, Long> {
+
+    @Query(value = "SELECT DISTINCT ROOM_TYPE FROM ROOM", nativeQuery = true)
+    public List<String> findAllRoomTypes();
+
+    @Query(value = "SELECT * FROM ROOM where BOOKING_ID IS null", nativeQuery = true)
+    public List<Room> findAvailableRooms();
+}
