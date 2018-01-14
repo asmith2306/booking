8d4e1571ab68f57f543c23f8c7ee9e98f184517a
Alan <Alan@Home-Laptop.Home>
2018-01-14 21:46:31 +0000

    Added room functionality

--- booking-controllers/src/main/java/com/asmith/booking/controllers/RoomController.java
+++ booking-controllers/src/main/java/com/asmith/booking/controllers/RoomController.java
@@ -1,7 +1,7 @@
 package com.asmith.booking.controllers;
 
 import com.asmith.booking.entities.Room;
-import com.asmith.booking.domain.beans.DomainBean;
+import com.asmith.booking.domain.beans.RoomDomainBean;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,7 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomController {
 
     @Autowired
-    DomainBean<Room> roomBean;
+    RoomDomainBean roomBean;
+
+    @RequestMapping(value = "/next", method = RequestMethod.GET)
+    ResponseEntity<Room> next(@RequestParam("type") String type) {
+        return new ResponseEntity<>(roomBean.next(type), HttpStatus.OK);
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     ResponseEntity<Room> read(@PathVariable("id") String id) {
@@ -28,7 +33,22 @@ public class RoomController {
         return new ResponseEntity<>(roomBean.find(id), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/available", method = RequestMethod.GET)
+    ResponseEntity<List<Room>> available() {
+        return new ResponseEntity<>(roomBean.findAvailableRooms(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/availableTypes", method = RequestMethod.GET)
+    ResponseEntity<List<String>> availableTypes() {
+        return new ResponseEntity<>(roomBean.findAvailableRoomTypes(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/types", method = RequestMethod.GET)
+    ResponseEntity<List<String>> types() {
+        return new ResponseEntity<>(roomBean.findAllRoomTypes(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
     ResponseEntity<List<Room>> readAll() {
 
         return new ResponseEntity<>(roomBean.findAll(), HttpStatus.OK);
